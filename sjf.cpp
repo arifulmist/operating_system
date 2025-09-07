20a41040615ba94a38e5718245a9f2b1594face7
Md Ariful Islam <154077378+arifulmist@users.noreply.github.com>
2025-09-08 01:46:43 +0600

    Add files via upload

---
+++ sjf.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll n;
+    cin >>n;
+    vector<ll> at(n), bt(n), ct(n), tat(n), wt(n), exc(n);
+    for (ll i = 0; i < n;i++)
+    {
+        cout << "enter your arrival time and burst time" << i + 1 << ": ";
+        cin >> at[i] >> bt[i];
+    }
+    ll tmp = n;
+    ll cur = 0;
+    while(tmp!=0)
+    {
+        ll mn = 1e9, ind = -1;
+        for (ll i = 0; i < n;i++)
+        {
+         if(cur>=at[i]&&bt[i]<mn&&exc[i]!=1)
+         {
+            mn= bt[i];
+            ind =i;
+         }
+       }
+       ct[ind] = cur + bt[ind];
+       tat[ind] = ct[ind] - at[ind];
+       wt[ind] = tat[ind] - bt[ind];
+       cur = ct[ind];
+       exc[ind] = 1;
+       tmp--;
+    }
+    double avgtat = 0, avgwt = 0;
+    for (ll i = 0; i < n;i++)
+    {
+        cout << "P" << i + 1 << "\t" << at[i] << "\t" << bt[i] << "\t" << ct[i]
+             << "\t" << tat[i] << "\t" << wt[i] << endl;
+        avgtat += tat[i];
+        avgwt += wt[i];
+    }
+    avgtat /= n;
+    avgwt /= n;
+    cout << "Average Turn Around Time: " << avgtat << "s\n";
+    cout << "Average Waiting Time: " << avgwt << "s\n";
+}
